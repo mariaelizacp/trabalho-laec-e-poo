8b805ce6f1e5cdc230d6a877d6782ea27b80503e
Maria Eliza Carollo Pina <44511830+mariaelizacp@users.noreply.github.com>
2018-11-06 15:20:23 -0300

    Inicializando arquivos do trabalho

---
+++ funcao.js
@@ -0,0 +1,63 @@
+function botaoum ()
+{
+	document.querySelector('#primeira').className="hidden";
+	document.querySelector('#login').className="visivel";
+	document.querySelector('#cadastro').className="hidden";
+	document.querySelector('#mapa').className="hidden";
+	document.querySelector('#registro').className="hidden";
+	document.querySelector('#feed').className="hidden";
+}
+function cadastro()
+{
+	document.querySelector('#primeira').className="hidden";
+	document.querySelector('#login').className="hidden";
+	document.querySelector('#cadastro').className="visivel";
+	document.querySelector('#mapa').className="hidden";
+	document.querySelector('#registro').className="hidden";
+	document.querySelector('#feed').className="hidden";
+}
+function cadastrar()
+{
+	document.querySelector('#primeira').className="hidden";
+	document.querySelector('#login').className="hidden";
+	document.querySelector('#cadastro').className="hidden";
+	document.querySelector('#mapa').className="visivel";
+	document.querySelector('#registro').className="hidden";
+	document.querySelector('#feed').className="hidden";
+}
+function registrar()
+{
+	document.querySelector('#primeira').className="hidden";
+	document.querySelector('#login').className="hidden";
+	document.querySelector('#cadastro').className="hidden";
+	document.querySelector('#mapa').className="hidden";
+	document.querySelector('#registro').className="visivel";
+	document.querySelector('#feed').className="hidden";
+}
+function registro()
+{
+	document.querySelector('#primeira').className="hidden";
+	document.querySelector('#login').className="hidden";
+	document.querySelector('#cadastro').className="hidden";
+	document.querySelector('#mapa').className="hidden";
+	document.querySelector('#registro').className="hidden";
+	document.querySelector('#feed').className="visivel";
+}
+function feed()
+{
+	document.querySelector('#primeira').className="hidden";
+	document.querySelector('#login').className="hidden";
+	document.querySelector('#cadastro').className="hidden";
+	document.querySelector('#mapa').className="hidden";
+	document.querySelector('#registro').className="hidden";
+	document.querySelector('#feed').className="visivel";
+}
+function entrar()
+{
+	document.querySelector('#primeira').className="hidden";
+	document.querySelector('#login').className="hidden";
+	document.querySelector('#cadastro').className="hidden";
+	document.querySelector('#mapa').className="hidden";
+	document.querySelector('#registro').className="hidden";
+	document.querySelector('#feed').className="visivel";
+}
